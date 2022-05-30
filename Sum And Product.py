042ebf0e3390a1a94136ab20ab91845f7db3378c
vaishnavi2809 <106094504+vaishnavi2809@users.noreply.github.com>
2022-05-31 00:04:05 +0530

    Add files via upload

---
+++ Sum And Product.py
@@ -0,0 +1,10 @@
+# Write a Python Program to calculate sum and product of an iterable object.
+
+import math as m
+a=eval(input("Enter any iterabe object"))
+sum=0
+prod=1
+sum=m.fsum(a)
+prod=m.prod(a)
+print(sum)
+print(prod)
